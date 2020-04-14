b3052603babca033298b8eef6d87840ff425cdb2
luk <schwarzblade@gamil.com>
2020-04-14 16:47:02 +0200

    add item to list

---
+++ app.js
@@ -0,0 +1,69 @@
+//selectors
+const todoInput = document.querySelector('.todo-input')	
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+//event listener
+
+
+todoButton.addEventListener('click',addItem);
+
+
+todoList.addEventListener('click', deleteCheck);
+
+
+//functions
+
+
+
+function addItem(e){
+	e.preventDefault();
+
+	const todoDiv = document.createElement("div");
+	todoDiv.classList.add("todo");
+
+	//create li
+
+	const newTodo = document.createElement("li");
+	newTodo.innerText = todoInput.value;
+	newTodo.classList.add("todo-item");
+
+	todoDiv.appendChild(newTodo);
+
+	//check mark button
+	const completedBtn = document.createElement("button");
+	completedBtn.innerHTML = '<i class="fas fa-check"></i>';
+	completedBtn.classList.add('complete-btn');
+	todoDiv.appendChild(completedBtn);
+
+	//delete btn
+	const trashBtn = document.createElement("button");
+	trashBtn.innerHTML = '<i class="fas fa-trash">';
+	trashBtn.classList.add('trash-btn');
+	todoDiv.appendChild(trashBtn);
+
+	//apend tolist
+
+	todoList.appendChild(todoDiv);
+
+
+	//clear input 
+	
+	todoInput.value="";
+
+}
+
+
+function deleteCheck(e){
+	const item = e.target; 
+	//delete todo
+	if(item.classList[0] === 'trash-btn'){
+		item.parentElement.remove()
+	}
+
+	//checkmark
+
+	if(item.classList[0] === 'complete-btn'){
+		item.parentElement.classList.toggle('completed');
+	}
+}
